ad793520083dae90645314cbc8345304154e7ec3
Lukas Calmbach <lcalmbach@gmail.com>
2024-02-26 08:20:40 +0100

    add st_player to gui

--- app.py
+++ app.py
@@ -4,21 +4,35 @@ from langchain.document_loaders.parsers import OpenAIWhisperParser
 from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
 from pytube import YouTube
 import os
+from streamlit_player import st_player
 
-save_dir="./docs/youtube/"
-url = st.text_input("Enter the URL of the YouTube video")
+__version__ = '0.0.1'
+__author__ = 'lukas.calmbach@bs.ch'
 
-if st.button("Transcribe"):
-    yt = YouTube(url)
-    with st.spinner(f"Transcribing '{yt.title}'..."):
+SAVE_DIR = './docs/youtube/'
+if 'url' not in st.session_state:
+    st.session_state['url'] = 'https://youtu.be/cfLrQZVpNWg?si=8wSsJQ26_SrNWwk0'
+    st.session_state['text'] = None
+url = st.text_input('Gib eine Youtube Video URL ein', st.session_state['url'])
+if url != st.session_state['url']:
+    st.session_state['url'] = url
+    st.session_state['text'] = None
+if st.session_state['url'] is not None:
+    yt = YouTube(st.session_state['url'])
+    st_player(st.session_state['url'])
+
+if st.button('Transkribiere Video'):
+    with st.spinner(f'Transcribing "{yt.title}"...'):
         loader = GenericLoader(
-            YoutubeAudioLoader([url], save_dir),
+            YoutubeAudioLoader([url], SAVE_DIR),
             OpenAIWhisperParser()
         )
         docs = loader.load()
-        st.write(docs[0].page_content)
-        text = ' '.join([page.page_content for page in docs])
-        st.write(text)
-        with open(os.path.join(save_dir, yt.title + '.txt'), "w") as file:
-            file.write(text)
-        st.success(f"Transcription of '{yt.title}' complete.")
+        st.session_state['text'] = ' '.join([page.page_content for page in docs])
+        with open(os.path.join(SAVE_DIR, yt.title + '.txt'), 'w', encoding='utf8') as file:
+            file.write(st.session_state['text'])
+        st.success(f'Transkription für "{yt.title}" ist komplett.')
+
+if st.session_state['text'] is not None:
+    st.write(st.session_state['text'])
+    st.download_button('Datei herunterladen', st.session_state['text'], f'{yt.title}.txt')
