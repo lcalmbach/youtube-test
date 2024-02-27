4b33619206d6249d400fe75de1cc101642855bec
Lukas Calmbach <lcalmbach@gmail.com>
2024-02-27 07:24:46 +0100

    include random url selector

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ from pytube import YouTube
 import os
 from streamlit_player import st_player
 import re
+import shutil
+import random
 
 __version__ = '0.0.1'
 __author__ = 'lukas.calmbach@bs.ch'
 SAVE_DIR = './docs/youtube/'
+
+url_list = ['https://youtu.be/cfLrQZVpNWg?si=8wSsJQ26_SrNWwk0',
+            'https://youtu.be/3rQWzPHi7-Y?si=zth7GypUAHlY_58o',
+            'https://youtu.be/vsZW2gUATG8?si=3dZn0tSPWKu8lSo1']
 
 
 def clean_filename(filename):
@@ -18,8 +24,17 @@ def clean_filename(filename):
     return cleaned_filename
 
 
+def delete_all_files_in_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.remove(file_path)  # Delete the file
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+            
+
 if 'url' not in st.session_state:
-    st.session_state['url'] = 'https://youtu.be/cfLrQZVpNWg?si=8wSsJQ26_SrNWwk0'
+    st.session_state['url'] = random.choice(url_list)
     st.session_state['text'] = None
     st.session_state['txt_file_name'] = None
 url = st.text_input('Gib eine Youtube Video URL ein', st.session_state['url'])
@@ -33,6 +48,7 @@ if st.session_state['url'] is not None:
 
 if st.button('Transkribiere Video'):
     with st.spinner(f'Transcribing "{yt.title}"...'):
+        delete_all_files_in_folder(SAVE_DIR)
         loader = GenericLoader(
             YoutubeAudioLoader([url], SAVE_DIR),
             OpenAIWhisperParser()
